728347624853a45d4080478866663af50f354e1f
Florian Holzner <florian@holzner.tech>
2022-10-24 18:08:29 +0200

    Started working on the lambda function.

--- lambda/telemetry.py
+++ lambda/telemetry.py
@@ -1,7 +1,17 @@
 import boto3
+import json
+
+dynamodb =  boto3.resource('dynamodb')
+table = dynamodb.Table('telemetry')
 
 def collect_telemetry(event, context):
     """
-    Handles incoming events.
+    Handle incoming events by storing valid JSON telemetry into DynamoDB.
     """
-    ...
+    try:
+        json.loads(event)
+    except json.JSONDecodeError:
+        # Do nothing for now. Maybe write to websockter later.
+        # Final form: the AWS logging thingy?
+        ...
+    
