6f2e6d7683b359fb65cd975dd89ad8592c3e0934
Florian Holzner <florian@holzner.tech>
2022-10-24 16:35:14 +0200

    Moved config of aws provider to main file.

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,7 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  region = "eu-central-1"
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
 resource "aws_apigatewayv2_api" "telemetry_collection_endpoint" {
   name          = "telemetry-example-telemetry-collection-endpoint"
   description   = "The API Gateway V2 Websocket for the Telemetry Collection Example"
