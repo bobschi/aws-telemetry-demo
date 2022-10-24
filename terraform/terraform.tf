76c48a705422df74b4eea6167820db08d2c43c09
Florian Holzner <florian@holzner.tech>
2022-10-24 16:32:57 +0200

    Extracted terraform section to main.tf file.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.2.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "eu-central-1"
 }
